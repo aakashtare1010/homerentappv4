28fdba313edf99b85f68af4f5af0dc7eb5c61f52
aavisha1010 <aakashtare1010@outlook.com>
2024-07-16 09:54:31 +0530

    first time add project architecture

---
+++ homerentappv4/src/main/java/com/rent/Homerentappv4Application.java
@@ -0,0 +1,13 @@
+package com.rent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homerentappv4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Homerentappv4Application.class, args);
+	}
+
+}
