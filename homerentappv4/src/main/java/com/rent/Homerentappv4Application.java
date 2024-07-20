feab0b0e9103c630a13b99350f4701c1eb396303
aavisha1010 <aakashtare1010@outlook.com>
2024-07-20 21:55:11 +0530

    dont know

--- homerentappv4/src/main/java/com/rent/Homerentappv4Application.java
+++ homerentappv4/src/main/java/com/rent/Homerentappv4Application.java
@@ -9,6 +9,7 @@ public class Homerentappv4Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Homerentappv4Application.class, args);
 		System.out.println("Hello Developer !!");
+		System.out.println("wELCOME TO hOME rENT aPP !!");
 	}
 
 }
